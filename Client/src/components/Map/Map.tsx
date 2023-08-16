07e48677aa7886a17111c4f04b2bf9cfffdab25e
yen-lei <97564546+yen-lei@users.noreply.github.com>
2023-08-16 15:49:55 -0700

    add basic mapbox

--- Client/src/components/Map/Map.tsx
+++ Client/src/components/Map/Map.tsx
@@ -1,5 +1,29 @@
+'use client';
+
 import "./Map.css";
 
+import mapboxgl from '!mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import React, { useRef, useEffect, useState } from 'react';
+ 
+mapboxgl.accessToken = 'MAP_TOKEN';
+
 export default function Map() {
-  return <div className="map_container"></div>;
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return <div ref={mapContainer} className="map_container" />;
 }
