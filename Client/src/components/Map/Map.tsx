d54cf0e878e3bbbdef464a8eeb2e7ed5a7357c3d
yen-lei <97564546+yen-lei@users.noreply.github.com>
2023-08-16 23:03:01 -0700

    remove extra small details from map; change colors

--- Client/src/components/Map/Map.tsx
+++ Client/src/components/Map/Map.tsx
@@ -13,16 +13,23 @@ export default function Map() {
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(3);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: 'mapbox://styles/yenlei/cllepzpeh00ha01pwed4dhdh0',  // map styling here; streets and other miscellaneous stuff were removed here
       center: [lng, lat],
       zoom: zoom
     });
+
+    // add zoom control
+    const nav = new mapboxgl.NavigationControl({
+      showCompass: false  // do not show compass controls so rotation of the map is not allowed (not neeeded anyway)
+    });
+    map.current.addControl(nav, "top-right");
+
   });
 
   return <div ref={mapContainer} className="map_container" />;
