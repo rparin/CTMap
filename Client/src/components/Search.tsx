8cce5507f1d8d27668a5d4a715721506d8f9d17c
Ralph Parin <trydonexz77@gmail.com>
2023-08-18 14:47:20 -0700

    add comment: location todo

--- Client/src/components/Search.tsx
+++ Client/src/components/Search.tsx
@@ -31,6 +31,8 @@ export default function Search() {
         onKeyDown={handleKeyDown}
       />
 
+      {/* Todo Location search: Search by address, city, state, or country */}
+
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleClick}>
