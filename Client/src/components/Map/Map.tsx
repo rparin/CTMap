cb184d1c85372faa0815c0906dc332514a26fa04
Ralph Parin <r.parin070@gmail.com>
2023-08-19 20:08:02 -0700

    update syntax to adhere to ts checks

--- Client/src/components/Map/Map.tsx
+++ Client/src/components/Map/Map.tsx
@@ -1,16 +1,17 @@
-'use client';
+"use client";
 
 import "./Map.css";
 
-import mapboxgl from '!mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css'
-import React, { useRef, useEffect, useState } from 'react';
- 
-mapboxgl.accessToken = 'MAP_TOKEN';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useRef, useEffect, useState } from "react";
+
+mapboxgl.accessToken =
+  "MAP_TOKEN";
 
 export default function Map() {
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  const map: any = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(3);
@@ -18,18 +19,17 @@ export default function Map() {
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/yenlei/cllepzpeh00ha01pwed4dhdh0',  // map styling here; streets and other miscellaneous stuff were removed here
+      container: mapContainer.current!,
+      style: "mapbox://styles/yenlei/cllepzpeh00ha01pwed4dhdh0", // map styling here; streets and other miscellaneous stuff were removed here
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
     });
 
     // add zoom control
     const nav = new mapboxgl.NavigationControl({
-      showCompass: false  // do not show compass controls so rotation of the map is not allowed (not neeeded anyway)
+      showCompass: false, // do not show compass controls so rotation of the map is not allowed (not neeeded anyway)
     });
     map.current.addControl(nav, "top-right");
-
   });
 
   return <div ref={mapContainer} className="map_container" />;
