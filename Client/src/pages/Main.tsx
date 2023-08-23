95085e387f9997b81d5e211396a8dc5e3fb9969b
Ralph Parin <r.parin070@gmail.com>
2023-08-22 18:56:37 -0700

    move search component to map

--- Client/src/pages/Main.tsx
+++ Client/src/pages/Main.tsx
@@ -1,16 +1,10 @@
 import "./Main.css";
 import Map from "@/components/Map/Map";
 import Results from "@/components/Results";
-import Search from "@/components/Search";
 
 export default function Main() {
   return (
     <main className="main_container">
-      {/* Top stuff */}
-      <div className="flex justify-between">
-        <Search />
-      </div>
-
       {/* Map */}
       <Map />
 
