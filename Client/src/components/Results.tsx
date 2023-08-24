4782845d34d0f6e8d6f3e71a4ed7996d00e0ca2f
Ralph Parin <r.parin070@gmail.com>
2023-08-23 18:27:16 -0700

    create box for results

--- Client/src/components/Results.tsx
+++ Client/src/components/Results.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-
 export default function Results() {
-  const [message, setMessage] = useState("Server not loaded");
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home").then((res) =>
-      res.json().then((data) => {
-        setMessage(data.message);
-      })
-    );
-  }, []);
-
-  return <div className="">{message}</div>;
+  return <div className="bg-slate-200 w-96 h-[40rem]"></div>;
 }
