08c724e0b89c8c5e6351a47aec3d3104f397485e
Ralph Parin <r.parin070@gmail.com>
2023-08-28 18:15:17 -0700

    display popup info

--- Client/src/components/MPopup.tsx
+++ Client/src/components/MPopup.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 export type pInfo = {
+  id: string;
   title: string;
-  details: string;
+  studyStart: string;
+  studyType: string;
+  phase: string;
 };
 
 export default function MPopup(info: pInfo) {
   return (
-    <div>
+    <section>
+      <p>{info.id}</p>
       <h3>{info.title}</h3>
-      <p>{info.details}</p>
-    </div>
+      <p>{info.studyStart}</p>
+      <p>{info.studyType}</p>
+      <p>{info.phase}</p>
+    </section>
   );
 }
