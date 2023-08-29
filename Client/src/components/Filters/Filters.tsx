be007f4a698e1ec3baf84376614d37058638828b
Maxine <mgjypil@gmail.com>
2023-08-28 17:30:35 -0700

    added basic styling

--- Client/src/components/Filters/Filters.tsx
+++ Client/src/components/Filters/Filters.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import "./Filters.css";
 import EligibilityCriteria from "./EligibilityCriteria";
 import StudyPhase from "./StudyPhase";
 import StudyType from "./StudyType";
@@ -19,8 +19,10 @@ export default function Filters() {
         <StudyType />
         <StudyResults />
 
-        <button type="button" id="clear-button" onClick={handleClear}>Clear Filters</button>
-        <button type="button" id="apply-button" onClick={handleApply}>Apply Filters</button>
+        <div id="filter-buttons">
+            <button type="button" id="clear-button" onClick={handleClear}>Clear Filters</button>
+            <button className ="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" id="apply-button" onClick={handleApply}>Apply Filters</button>
+        </div>
     </form>
     );
 }
