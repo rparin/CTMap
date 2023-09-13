5aeb8ba073f10c482937df103e445070a1d0d144
Maxine <74639009+mgjypil@users.noreply.github.com>
2023-09-13 16:01:34 -0700

    Main css to Tailwind

--- Client/src/pages/Main.tsx
+++ Client/src/pages/Main.tsx
@@ -3,7 +3,7 @@ import Map from "@/components/Map/Map";
 
 export default function Main() {
   return (
-    <main className="main_container">
+    <main className="flex flex-col min-h-screen gap-2 text-white bg-gray-500">
       {/* Map */}
       <Map />
 
